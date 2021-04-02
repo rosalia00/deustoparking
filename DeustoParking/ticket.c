496a0a75ec1cd9ed3bfceb8928e510648313ceaf
Rosalia Perez <rosalia.perez@opendeusto.es>
2021-04-02 22:09:51 +0200

    TIcket 2

--- DeustoParking/ticket.c
+++ DeustoParking/ticket.c
@@ -9,7 +9,7 @@
  * Creamos una estructura que recibira los datos
  * para meter los datos esta clase tendrá que ser llamada en las que esten los dato s
  */
-struct reserva{
+typedef struct{
 	char nombre[30];
 	char apellido[40];
 	char dni[9];
@@ -19,25 +19,32 @@ struct reserva{
 	float precio;
 	char matricula[12];
 	int plaza;
-};
+}reserva;
 
-struct reserva reserva_t;
+reserva reserva_t;
 
 
 /*
  * Guarda en fichero
  */
-main(){
 
-void ticket(struct reserva *r){
+void ticket(reserva *res){
 	FILE *freserva;
 	freserva = fopen("ticket.dat", "a");
-	if (freserva == NULL){
-		printf("No se puede abrir el fichero");
+	if (freserva != NULL){
+		fwrite(&res, sizeof(reserva), 1, freserva);
+		close(freserva);
+		//si no functiona co sizeof(reserva) probar sizeof(res)
 	}
-	else {
-
-	}
-}
-
+	//res = &res_t;
+	printf("******DATOS DE LA RESERVA****** \n");
+	printf("Nombre: %s \n", res->nombre);
+	printf("Apellido: %s \n", res->apellido);
+	printf("DNI: %i \n", res->dni);
+	printf("Tarjeta: %i \n", res->tarjeta);
+	printf("Fecha de Inicio: %s \n", res->datainicio);
+	printf("Fecha de Fin: %s \n", res->datafin);
+	printf("Precio: %.2f \n", res->precio);
+	printf("Matricula: %s \n", res->matricula);
+	printf("Plaza: %i \n", res->plaza);
 }
