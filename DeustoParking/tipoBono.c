8b407caf5c3035928d3862a6205ce86e43011057
Rosalia Perez <rosalia.perez@opendeusto.es>
2021-04-06 20:41:55 +0200

    tipo bonos modelo casi terminada

--- DeustoParking/tipoBono.c
+++ DeustoParking/tipoBono.c
@@ -5,13 +5,49 @@
  *      Author: rosal
  */
 
-void tipoBono() {
-//Menu compra bonos
-	printf("\n Elija tipo de bono: ");
-	printf("\n 1. Bono Semanal Alumnado: 62,99 €");
-	printf("\n 2. Bono Semanal Profesorado: 75,59 €");
-	printf("\n 3. Bono Mensual Alumnado: 209,99 €");
-	printf("\n 4. Bono Mensual Profesorado: 251,99 €");
-	printf("\n 5. Bono Anual Alumnado: 1641,99 €");
-	printf("\n 6. Bono Anual Profesorado: 1970,39 €");
+#include <stdio.h>
+
+void tipoBono(){
+	int op=0;
+	float tipoBono;
+	char c;
+
+	do{
+	printf("\n TIPOS DE BONO: ");
+	printf("\n 1. Bono Semanal Alumnado: 62,99 â‚¬. ");
+	printf("\n 2. Bono Semana Profesorado: 75,59 â‚¬. ");
+	printf("\n 3. Bono Mensual Alumnado: 209,99 â‚¬. ");
+	printf("\n 4. Bono Mensual Profesorado: 251,99 â‚¬. ");
+	printf("\n 5. Bono Anual Alumnado: 1.641,99 â‚¬. ");
+	printf("\n 6. Bono Anual Profesorado: 1970,39 â‚¬ ");
+	printf("\n Elija tipo de Bono: ");
+	scanf("%d", &op);
+	switch (op)
+		{
+	case 1:
+		tipoBono = 62,99;
+		return tipoBono;
+		break;
+	case 2:
+		tipoBono = 75,59;
+		return tipoBono;
+		break;
+	case 3:
+		tipoBono = 62,99;
+		return tipoBono;
+		break;
+	default:
+		printf('\n Opción Errónea... Pulse una tecla. ');
+		c = getchar();
+
+		break;
+		} (op >=1 || op<=6)
+
+
+	}while(op <=0 && op>=0);
 }
+
+
+
+
+
