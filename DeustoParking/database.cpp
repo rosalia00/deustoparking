6cd48b6cb871f9e2261fb41384e98874ddab7578
Tyler de Mier <tylerdemier@opendeusto.es>
2021-05-18 15:46:18 +0200

    funcion administrar bonos en base de datos + recogida de errores.

--- DeustoParking/database.cpp
+++ DeustoParking/database.cpp
@@ -4,6 +4,8 @@ extern "C" {
 }
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
+using namespace std;
 
 static int callback(void *data, int argc, char **argv, char **azColName) {
 	int i;
@@ -23,7 +25,8 @@ int cuentaReservas(sqlite3 *db) {
 	int rc;
 	int numReserv = 0;
 
-	int result = sqlite3_exec(db, "SELECT COUNT(*) FROM RESERVA", callback, &numReserv, &zErrMsg);
+	int result = sqlite3_exec(db, "SELECT COUNT(*) FROM RESERVA", callback,
+			&numReserv, &zErrMsg);
 	if (result) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
@@ -37,10 +40,62 @@ int cuentaReservas(sqlite3 *db) {
 
 void recogeReservas(Reserva*, sqlite3 *db) {
 
-
 }
 
-sqlite3* db;
+/* --- ADMINISTRAR BONOS --- */
+int administrarBono(sqlite3 *db, float precio, int opcion) {
+
+	sqlite3_stmt *stmt;
+
+	char sql[] = "UPDATE BONO SET PRECIO = ? WHERE ID = ?";
+
+	int resultado = sqlite3_prepare_v2(db, sql, strlen(sql), &stmt, NULL);
+	if (resultado != SQLITE_OK) {
+		cout << "Error preparando la declaración (UPDATE)" << endl
+				<< sqlite3_errmsg(db) << endl;
+		return resultado;
+	}
+
+	cout << "Consulta SQL preparada (UPDATE)" << endl;
+
+	resultado = sqlite3_bind_double(stmt, 1, precio);
+	if (resultado != SQLITE_OK) {
+		cout << "Error uniendo el parametro precio" << endl
+				<< sqlite3_errmsg(db);
+		return resultado;
+	}
+
+	resultado = sqlite3_bind_int(stmt, 2, opcion);
+	if (resultado != SQLITE_OK) {
+		cout << "Error uniendo el parametro opción" << endl
+				<< sqlite3_errmsg(db) << endl;
+		return resultado;
+	}
+
+	cout << "Parametros insertados" << endl;
+
+	resultado = sqlite3_step(stmt);
+	if (resultado == SQLITE_OK) {
+		cout << "Error actualizando los datos." << endl << sqlite3_errmsg(db)
+				<< endl;
+		return resultado;
+	}
+
+	cout << "Datos actualizados" << endl;
+
+	resultado = sqlite3_finalize(stmt);
+	if (resultado != SQLITE_OK) {
+		cout << "Error terminando la declaracion (UPDATE)" << endl
+				<< sqlite3_errmsg(db) << endl;
+		return resultado;
+	}
+
+	cout << "La declaración preparada ha terminado (UPDATE)" << endl;
+
+	return SQLITE_OK;
+}
+
+sqlite3 *db;
 int rc = sqlite3_open("test.db", &db);
 int a = cuentaReservas(db)
 
