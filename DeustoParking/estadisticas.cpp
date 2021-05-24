86ae7ee33a3fd25f139be34638093b69efd2bd30
Rosalia Perez <rosalia.perez@opendeusto.es>
2021-05-25 00:22:29 +0200

    ESTADISTICAS: ideas de algunas estadisticas que podemos tener

--- DeustoParking/estadisticas.cpp
+++ DeustoParking/estadisticas.cpp
@@ -1,2 +1,15 @@
+#include "estadisticas.h"
 
+/*estadistica 1: selecciona el precio medio de los bonos para comporaciones con otras empresas
+ * SELECT AVG(PRECIO) FROM BONO
+ *
+ * o selecciona precio medio de reservas
+ * SELECT AVG(PRECIO) FROM RESERVA
+ */
 
+/* estadistica 2: precio más alto de las reservas
+ * SELECT MAX(PRECIO) FROM RESERVA
+ *
+ * o selecciona precio maximo de un usuario
+ * SLEECT MAX(PRECIO) FROM RESERVA WHERE DNI="dni usuario"
+ */
---
+++ DeustoParking/estadisticas.h
@@ -0,0 +1,13 @@
+/*
+ * estadisticas.h
+ *
+ *  Created on: 24 may. 2021
+ *      Author: rosal
+ */
+
+#ifndef ESTADISTICAS_H_
+#define ESTADISTICAS_H_
+
+
+
+#endif /* ESTADISTICAS_H_ */
